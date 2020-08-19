9abde95790e380767d90a90fcb90b759176fafa7
Anthony Alcorn <xmastony@gmail.com>
2020-08-19 06:14:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -192,6 +192,19 @@ player_stats = nil
 	player_stats
 end
 
+def big_shoe_rebounds
 
+  biggest_shoe = 0
+  player_rebounds = 0
+  game_hash.values.each do |team_info|
+    team_info[:players].each do |player|
+      if player[:shoe] > biggest_shoe
+        biggest_shoe = player[:shoe] 
+        player_rebounds = player[:rebounds] 
+      end
+    end
+  end
+  return player_rebounds
+end
 
 # Write code here
