3fb40e04d9f0493e0bb000fdcf9ee34844dbdbf5
Anthony Alcorn <xmastony@gmail.com>
2020-08-19 06:37:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -190,16 +190,6 @@ def player_numbers(team) #WORKS
   end
 end
 
-def all_stats_for_player(player_name, game)
-  game.each do |team, team_hash|
-    team_hash[:players].each do |player, player_hash|
-      if player_hash[:name] == player_name
-        return player_hash[:stats]
-      end
-    end
-  end
-end
-
 
 
 # Write code here
