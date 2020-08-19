a33e7c75a7c658b7e03392433553fbb4cb483223
Anthony Alcorn <xmastony@gmail.com>
2020-08-19 06:08:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,18 +127,7 @@ def game_hash
   }
 end
 
-def num_points_scored(player_name)
-	player_points = nil
-	game_hash.each do |location, team_data|
-		
-		game_hash[location][:players].each do |player, stats|
-            	if stats[:player_name] == player_name
-            		player_points = stats[:points]
-            	end
-		end	
-	end
-	player_points.to_i
-end
+
 
 def shoe_size(player_name)
 	player_shoe_size = nil
