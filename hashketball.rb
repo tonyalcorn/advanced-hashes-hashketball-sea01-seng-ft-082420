04088457bb1f898653a41d719d967b8906cab772
Anthony Alcorn <xmastony@gmail.com>
2020-08-19 06:01:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -185,6 +185,17 @@ def player_numbers(team_name)
 	jersey_nums
 end
 
-
+def player_stats(player_name)
+player_stats = nil
+	game_hash.each do |location, loc_data|
+		loc_data[:players].each do |player, stats|
+			if stats[:player_name] == player_name
+	           player_stats = stats
+	           player_stats.delete(:player_name)
+			end
+		end
+	end
+	player_stats
+end
 
 # Write code here
